15c60753b49dfc7d48ebca5a25be93450a1d6b81
Simone Bogni <simone.bogni@gmail.com>
2021-07-19 23:35:19 +0300

    Created product model, added it to the admin panel and made migration

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='products', default='no_picture.jpg')
+    price = models.FloatField(help_text='in Euros €')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}-{self.created_at.strftime('%Y-%m-%d')}"
