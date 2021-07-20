ac488aded2ee85b7056dd84e0459160504bbb6f6
Simone Bogni <simone.bogni@gmail.com>
2021-07-20 19:57:37 +0300

    Created view, templates and routes for sales homepage and list

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Sale
 # Create your views here.
+
+def home_view(request):
+    hello = 'Hello world from the view'
+    return render(request, 'sales/home.html', {'msg':hello})
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
