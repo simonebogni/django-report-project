4fa87dc5de7ec93597b3b7900bf2b54a85873afb
Simone Bogni <simone.bogni@gmail.com>
2021-07-21 15:12:49 +0300

    Created searchbar in sales homepage and printed pandas sales dataframe

--- sales/views.py
+++ sales/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
+import pandas as pd
 # Create your views here.
 
 def home_view(request):
-    hello = 'Hello world from the view'
-    return render(request, 'sales/home.html', {'msg':hello})
+    sales_df = None
+    form = SalesSearchForm(request.POST or None)
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+
+        qs = Sale.objects.filter(created_at__date__lte=date_to, created_at__date__gte=date_from)
+        if len(qs) > 0:
+            sales_df = pd.DataFrame(qs.values())
+            sales_df = sales_df.to_html()
+            print(sales_df)
+        else:
+            print('no data')
+        
+        
+    context = {
+        'form': form,
+        'sales_df': sales_df
+    }
+    return render(request, 'sales/home.html', context)
 
 class SaleListView(ListView):
     model = Sale
